c42c48020dc9b62d00566425682f4e172dd4e19d
harshitkakadiya <harshitkakadiya01@gmail.com>
2024-07-02 08:28:17 +0530

    02-072024

---
+++ MY_SQL/lecture03/lecture03.sql
@@ -0,0 +1,13 @@
+-- filter data
+select * from product_master where qtyonhand = 100;
+
+-- update
+update product_master set qtyonhand = 10 where description = 'Trousers';
+-- delete
+
+delete from product_master where description = 'Shirts';
+
+
+
+
+
